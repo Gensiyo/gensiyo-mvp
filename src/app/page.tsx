f5d414970de45d0771a71668d2e76fca59120f47
Gensiyo <info@gensiyo.com>
2025-08-10 20:35:07 +0200

    add new clean file

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-slate-900 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Gensiyo - Working</h1>
+    </div>
+  )
+}
